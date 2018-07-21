e595f6b158feb42d09762deeb8cebdad35117577
Andrés <Andrs@DESKTOP-685B185>
2018-07-21 20:17:57 +0200

    updated main with resources and its variables

---
+++ main.tf
@@ -0,0 +1,32 @@
+resource "azurerm_app_service" "app_service" {
+  name                = "${var.app_service_name}"
+  location            = "${data.azurerm_resource_group.rg.location}"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
+  app_service_plan_id = "${local.plan_id}"
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+  }
+
+  app_settings = "${var.app_settings}"
+}
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  count               = "${var.app_service_plan_id != "" ? 0 : 1}"
+  name                = "${var.app_service_plan_name}"
+  location            = "${data.azurerm_resource_group.rg.location}"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
+
+  sku {
+    tier = "${var.sku_tier}"
+    size = "${var.sku_size}"
+  }
+}
+
+locals {
+  plan_id = "${var.app_service_plan_id != "" ? var.app_service_plan_id : length(azurerm_app_service_plan.app_service_plan.*.id) > 0 ? element(concat(azurerm_app_service_plan.app_service_plan.*.id, list("")), 0) : ""}"
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "${var.resource_group_name}"
+}
---
+++ variable.tf
@@ -0,0 +1,30 @@
+variable "sku_tier" {
+  description = "App service SKU"
+}
+
+variable "sku_size" {
+  description = "App service SKU"
+}
+
+variable "resource_group_name" {
+  description = "Resource group name that the database will be created in."
+}
+
+variable "app_service_name" {
+  description = "App service name"
+}
+
+variable "app_service_plan_id" {
+  description = "App service plan id"
+  default     = ""
+}
+
+variable "app_service_plan_name" {
+  description = "App service plan name"
+  default     = "App_service_plan"
+}
+
+variable "app_settings" {
+  type    = "map"
+  default = {}
+}
